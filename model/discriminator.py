1cbdf8b54996d3c2efe8f0f57c79bce90dacc26f
pulkit48 <135384814+pulkit48@users.noreply.github.com>
2024-10-31 00:15:36 +0530

    Update discriminator.py

--- model/discriminator.py
+++ model/discriminator.py
@@ -68,39 +68,3 @@ class ConditionalDiscriminator(nn.Module):
     x=self.block5(x)
 
     return x
-import torch
-import torch.nn as nn
-
-class GeneratorLoss(nn.Module):
-
-  def __init__(self,alpha=100):
-    super().__init__()
-
-    self.alpha=alpha
-    self.bce=nn.BCEWithLogitsLoss()
-    self.l1=nn.L1Loss()
-
-  def forward(self,real,fake,fake_pred):
-
-    fake_target=torch.ones_like(fake_pred)
-    loss=self.bce(fake_pred,fake_target)+self.alpha*self.l1(real,fake)
-    return loss
-
-
-class DiscriminatorLoss(nn.Module):
-
-  def __init__(self):
-
-    super().__init__()
-
-    self.bce=nn.BCEWithLogitsLoss()
-
-  def forward(self,fake_pred,real_pred):
-    fake_target=torch.zeros_like(fake_pred)
-    real_target=torch.ones_like(real_pred)
-
-    fake_loss=self.bce(fake_pred,fake_target)
-    real_loss=self.bce(real_pred,real_target)
-
-    loss=(fake_loss+real_loss)/2
-    return loss
